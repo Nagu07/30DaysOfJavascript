1be0d56603c4de4d039fe92e04d9ca4e8e219a30
Nagu07 <naga00798@gmail.com>
2022-07-20 00:11:25 +0530

    initial excercise changes

---
+++ Day1/script.js
@@ -0,0 +1,20 @@
+// comments make code more readable
+// Welcome to 30DaysOfJavascript
+
+/*
+Comments make code readable,
+easy to reuse and informative
+*/
+
+// Four variables with assigned values
+const PI = 3.14
+const metric = 67.89
+const MathPercentage = 100
+const Subject = 'Math'
+
+// Four variables with unassigned values
+
+let myAge = 32
+let yourAge = 67
+console.log('I am '+myAge+' old')
+console.log(`You are ${yourAge} old`)
---
+++ Day1/variable.js
@@ -0,0 +1,14 @@
+let name = 'John'
+let number = 45
+let undefValue
+let emptyValue = null
+
+// Declare variables in multi line
+let firstName = 'Steve'
+let lastName = 'Jobs'
+let maritalStatus = 'Single'
+let country = 'USA'
+let age = 57
+
+// Declare variables ins single line
+let fName = 'Steve', lName = 'Jobs', status = 'Single', aage = 45
