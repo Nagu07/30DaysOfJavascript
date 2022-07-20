819d48f53f60da055459aba1fa891f78eb172dc6
Nagu07 <naga00798@gmail.com>
2022-07-20 22:54:41 +0530

    few more excercise for day1

---
+++ Day1/datatype.js
@@ -0,0 +1,7 @@
+console.log(typeof 'John') // String
+console.log(typeof 'c') // Will it detect a Char? No i think its always a string - string
+console.log(typeof 2) // Normal number // number
+console.log(typeof 2.589) // Tried a decimal // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
